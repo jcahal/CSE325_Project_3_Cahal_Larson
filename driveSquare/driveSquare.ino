c81383e425f9e52dff498e851200087352af377d
jcahal <jacahal@gmail.com>
2018-02-15 14:54:26 -0700

    back to default

--- driveSquare/driveSquare.ino
+++ driveSquare/driveSquare.ino
@@ -5,59 +5,22 @@ Servo carServo; // Create a Servo Object
 
 #define pwmToMotor 2        // Defining the PWM pin to run the motor
 #define servoPin 48         // Defining the servo control pin
-#define dirPin 3
-#define motorDir 0
 
-void turn(Servo servo) {
-
-  // turn the wheel
-  servo.write(180);
-  delay(250);
-
-  // drive a bit
-  analogWrite(pwmToMotor, 20);
-  delay(500);
-  analogWrite(pwmToMotor, 0);
-  delay(500);
-  
-  // straighten the wheel
-  servo.write(90);
-  delay(250);
-}
-
-void drive(int speed) {
-  analogWrite(pwmToMotor, speed);
-  delay(1000);
-  analogWrite(pwmToMotor, 0);
-  delay(1000);
-}
-
-void driveSquare(int speed, Servo servo) {    // Drive square function initial definition to drive a square
-  // loop through this 4 times
-  for(int i = 0; i <= 4; i++){
-    drive(speed);
-    turn(servo);
-  }
-
-}
-void carStop() {       // Stop function initial definition
-  
-}
+void      driveSquare();    // Drive square function initial definition to drive a square
+void      carStop();        // Stop function initial definition
 
 
 
-int carSpeed = 40;           // define a variable for speed of the car
+int carSpeed = 0;           // define a variable for speed of the car
 
 void setup()               // All the initail configuration should be placed here in setup 
 {
   carServo.attach(servoPin);  // setting the Servo motor control for the pin 
-  pinMode(dirPin, OUTPUT);
-  digitalWrite(dirPin, motorDir); // define forward motion always
 }
 
 void loop()                // Loop function which will be run forever
 {
-  driveSquare(carSpeed, carServo);          // drive a square
+  driveSquare();          // drive a square
   carStop();              // stop the car
   delay(10000);           // wait for 10 seconds
 }
@@ -65,7 +28,7 @@ void loop()                // Loop function which will be run forever
 
 
 
-/*
+
 void driveSquare() 
 {  
   // The code to drive the car in a square 
@@ -75,7 +38,6 @@ void carStop()
 {
   // The code to stop the car
 }
-*/
 
 
 
